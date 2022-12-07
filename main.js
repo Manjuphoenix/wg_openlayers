d3e2c6b507da2b8da52986e430e598a2a07b3af0
manjuphoenix <manjunath1055@gmail.com>
2022-12-07 16:04:35 +0545

    Westernghats all layers working except states. Sates layer not getting overlayed

--- main.js
+++ main.js
@@ -4,18 +4,64 @@ import TileWMS from 'ol/source/TileWMS';
 import View from 'ol/View';
 import OSM from 'ol/source/OSM';
 
-const style = new Style({
-  fill: new Fill({
-    color: '#eeeeee',
-  }),
-});
-
 
 var wg_villages = new TileLayer({
     title: 'villages',
     source: new TileWMS({
         url: 'http://localhost:8080/geoserver/WesternGhats/wms',
         params: {'LAYERS': 'WesternGhats:wg_villages', 'TILED': true},
+        serverType: 'geoserver',
+        visible: true
+    })
+});
+
+var wg_taluks = new TileLayer({
+    title: 'taluks',
+    source: new TileWMS({
+        url: 'http://localhost:8080/geoserver/WesternGhats/wms',
+        params: {'LAYERS': 'WesternGhats:wg_taluks', 'TILED': true},
+        serverType: 'geoserver',
+        visible: true
+    })
+});
+
+
+
+var wg_districts = new TileLayer({
+    title: 'districts',
+    source: new TileWMS({
+        url: 'http://localhost:8080/geoserver/WesternGhats/wms',
+        params: {'LAYERS': 'WesternGhats:wg_districts', 'TILED': true},
+        serverType: 'geoserver',
+        visible: true
+    })
+});
+
+var wg_states_layer = new TileLayer({
+    title: 'states',
+    source: new TileWMS({
+        url: 'http://localhost:8080/geoserver/WesternGhats/wms',
+        params: {'LAYERS': 'WesternGhats:wg_states', 'TILED': true},
+        serverType: 'geoserver',
+        visible: true
+    })
+});
+
+var wg_grids = new TileLayer({
+    title: 'districts',
+    source: new TileWMS({
+        url: 'http://localhost:8080/geoserver/WesternGhats/wms',
+        params: {'LAYERS': 'WesternGhats:wg_grids', 'TILED': true},
+        serverType: 'geoserver',
+        visible: true
+    })
+});
+
+var wg_westernghats = new TileLayer({
+    title: 'western_ghats',
+    source: new TileWMS({
+        url: 'http://localhost:8080/geoserver/WesternGhats/wms',
+        params: {'LAYERS': 'WesternGhats:western_ghats', 'TILED': true},
         serverType: 'geoserver',
         visible: true
     })
@@ -30,6 +76,168 @@ var wg_districts = new TileLayer({
         visible: true
     })
 });
+
+function westernghats() {
+    map.addLayer(wg_westernghats);
+    map.on('singleclick', function (evt) {
+  const viewResolution = /** @type {number} */ (view.getResolution());
+  // wg_villages.addFeature(selected_polygon_style);
+  var url = wg_westernghats.getSource().getFeatureInfoUrl(
+    evt.coordinate,
+    viewResolution,
+    'EPSG:3857',
+    {
+        'LAYERS': 'WesternGhats:western_ghats',
+    }
+  );
+  // console.log(url);
+  if (url) {
+    fetch(url)
+      .then((response) => response.text()
+      )
+      .then((text) => {
+        // document.getElementById('info').innerText = text;
+        // console.log(text);
+        var ESR = "";
+        console.log(text);
+        var index = text.indexOf('id');
+                var newstring = text.slice(index);
+                // console.log(newstring)
+                // var newindex1 = text.indexOf('IISC_WGESR');
+                // IISC_EWG = text.slice(newindex1);
+                ESR = newstring.split("\n");
+        document.getElementById('westernghats_area').innerText = ESR[1];
+        document.getElementById('westernghats_ar').innerText = ESR[2];
+      });
+  }
+});
+};
+
+function grids() {
+    map.addLayer(wg_grids);
+    map.on('singleclick', function (evt) {
+  const viewResolution = /** @type {number} */ (view.getResolution());
+  // wg_villages.addFeature(selected_polygon_style);
+  var url = wg_grids.getSource().getFeatureInfoUrl(
+    evt.coordinate,
+    viewResolution,
+    'EPSG:3857',
+    {
+        'LAYERS': 'WesternGhats:wg_grids',
+    }
+  );
+  // console.log(url);
+  if (url) {
+    fetch(url)
+      .then((response) => response.text()
+      )
+      .then((text) => {
+        // document.getElementById('info').innerText = text;
+        // console.log(text);
+        var ESR = "";
+        console.log(text);
+        var index = text.indexOf('id');
+                var newstring = text.slice(index);
+                // console.log(newstring)
+                // var newindex1 = text.indexOf('IISC_WGESR');
+                // IISC_EWG = text.slice(newindex1);
+                ESR = newstring.split("\n");
+        document.getElementById('grid_id').innerText = ESR[2];
+        document.getElementById('grid_nid').innerText = ESR[3];
+        document.getElementById('grid_intfsum').innerText = ESR[4];
+        document.getElementById('grid_forest18').innerText = ESR[5];
+        document.getElementById('grid_forest18p').innerText = ESR[6];
+        document.getElementById('grid_intfor').innerText = ESR[7];
+        document.getElementById('grid_elevmean').innerText = ESR[8];
+        document.getElementById('grid_slopemean').innerText = ESR[9];
+        document.getElementById('grid_state_name').innerText = ESR[10];
+        document.getElementById('grid_wgrainfall').innerText = ESR[11];
+        document.getElementById('grid_population').innerText = ESR[12];
+        document.getElementById('grid_tribal_pop').innerText = ESR[13];
+      });
+  }
+});
+};
+
+function taluks() {
+    map.addLayer(wg_taluks);
+    map.on('singleclick', function (evt) {
+  const viewResolution = /** @type {number} */ (view.getResolution());
+  // wg_villages.addFeature(selected_polygon_style);
+  var url = wg_taluks.getSource().getFeatureInfoUrl(
+    evt.coordinate,
+    viewResolution,
+    'EPSG:3857',
+    {
+        'LAYERS': 'WesternGhats:wg_taluks',
+    }
+  );
+  // console.log(url);
+  if (url) {
+    fetch(url)
+      .then((response) => response.text()
+      )
+      .then((text) => {
+        // document.getElementById('info').innerText = text;
+        // console.log(text);
+        var ESR = "";
+        console.log(text)
+        var index = text.indexOf('type_3');
+                var newstring = text.slice(index);
+                // console.log(newstring)
+                // var newindex1 = text.indexOf('IISC_WGESR');
+                // IISC_EWG = text.slice(newindex1);
+                ESR = newstring.split("\n");
+        document.getElementById('taluk_esa').innerText = ESR[1];
+        document.getElementById('taluk_state_name').innerText = ESR[2];
+        document.getElementById('taluk_dist_name').innerText = ESR[3];
+        document.getElementById('taluk_taluk_name').innerText = ESR[4];
+        document.getElementById('taluk_area').innerText = ESR[5];
+        document.getElementById('taluk_country').innerText = ESR[7];
+      });
+  }
+});
+};
+
+function wg_states() {
+    map.addLayer(wg_states_layer);
+    map.on('singleclick', function (evt) {
+  const viewResolution = /** @type {number} */ (view.getResolution());
+  // wg_villages.addFeature(selected_polygon_style);
+  var url = wg_states_layer.getSource().getFeatureInfoUrl(
+    evt.coordinate,
+    viewResolution,
+    'EPSG:3857',
+    {
+        'LAYERS': 'WesternGhats:wg_states',
+    }
+  );
+  // console.log(url);
+  if (url) {
+    fetch(url)
+      .then((response) => response.text()
+      )
+      .then((text) => {
+        // document.getElementById('info').innerText = text;
+        // console.log(text);
+        var ESR = "";
+        console.log(text)
+        var index = text.indexOf('type_3');
+                var newstring = text.slice(index);
+                // console.log(newstring)
+                // var newindex1 = text.indexOf('IISC_WGESR');
+                // IISC_EWG = text.slice(newindex1);
+                ESR = newstring.split("\n");
+        document.getElementById('taluk_esa').innerText = ESR[1];
+        document.getElementById('taluk_state_name').innerText = ESR[2];
+        document.getElementById('taluk_dist_name').innerText = ESR[3];
+        document.getElementById('taluk_taluk_name').innerText = ESR[4];
+        document.getElementById('taluk_area').innerText = ESR[5];
+        document.getElementById('taluk_country').innerText = ESR[7];
+      });
+  }
+});
+};
 
 function wg_village() {
     map.addLayer(wg_villages);
@@ -59,23 +267,30 @@ function wg_village() {
         // document.getElementById('info').innerText = text;
         // console.log(text);
         var ESR = "";
+        console.log(text)
         var index = text.indexOf('objectid');
                 var newstring = text.slice(index);
                 // console.log(newstring)
                 // var newindex1 = text.indexOf('IISC_WGESR');
                 // IISC_EWG = text.slice(newindex1);
                 ESR = newstring.split("\n");
-                // console.log(ESR[0]);
-                // console.log(ESR[1]);
-                // console.log(ESR[2]);
-                // console.log(ESR[3]);
-                // console.log(ESR[4]);
-                // console.log(ESR[5]);
-        document.getElementById('st_nm').innerText = ESR[2];
-        document.getElementById('tehs_nm').innerText = ESR[3];
-        document.getElementById('dist_nm').innerText = ESR[5];
-        document.getElementById('village_nm').innerText = ESR[18];
-        document.getElementById('esr').innerText = ESR[23];
+        document.getElementById('st_nm').innerText = ESR[1];
+        document.getElementById('tehs_nm').innerText = ESR[2];
+        document.getElementById('dist_nm').innerText = ESR[4];
+        document.getElementById('no_hh').innerText = ESR[5];
+        document.getElementById('tot_p').innerText = ESR[6];
+        document.getElementById('tot_m').innerText = ESR[7];
+        document.getElementById('tot_f').innerText = ESR[8];
+        document.getElementById('p_sc').innerText = ESR[9];
+        document.getElementById('p_st').innerText = ESR[10];
+        document.getElementById('p_lit').innerText = ESR[11];
+        document.getElementById('p_ill').innerText = ESR[12];
+        document.getElementById('tot_wk_p').innerText = ESR[13];
+        document.getElementById('tot_wk_m').innerText = ESR[14];
+        document.getElementById('tot_wk_f').innerText = ESR[15];
+        document.getElementById('orig_fid').innerText = ESR[16];
+        document.getElementById('village_nm').innerText = ESR[17];
+        document.getElementById('esr').innerText = ESR[22];
       });
   }
 });
@@ -148,6 +363,10 @@ function wg_district() {
 
 window.villages = wg_village;
 window.districts = wg_district;
+window.westernghats = westernghats;
+window.grids = grids;
+window.states = wg_states;
+window.taluks = taluks;
 
 const view = new View({
     center: [0, 0],
